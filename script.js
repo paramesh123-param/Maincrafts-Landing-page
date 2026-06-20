4a6bfba0ca65e8040565d14844d9dbfcf1ab74bc
paramesh123-param <parameshwarim205@gmail.com>
2026-06-20 08:52:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let email=document.getElementById("email").value;
 
 let message=document.getElementById("message").value;
 
+
 if(name=="" || email=="" || message==""){
 
 alert("Please fill all fields");
@@ -13,6 +14,7 @@ alert("Please fill all fields");
 return false;
 
 }
+
 
 alert("Message Sent Successfully!");
 
